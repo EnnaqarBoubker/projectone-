6acd28aa8ee3f3a5747a356a2833af13bf4d7319
Boubker Ennaqar <ennaqar.boubker5@gmail.com>
2024-06-28 18:08:21 +0100

    create project laravel

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// console.log('Hello boubker')
+
+var person1 = {
+	name: "foulan",
+	age: 9000,
+	isStudent: true
+}
+
+// console.log(person1)
+
+
+// console.log(1+3)
+// console.log(12/3)
+// console.log(3**4)
+// console.log(18%8)
+// console.log(0/0);
+
+var a = 10  // string
+var b = 10 // string
+
+console.log(typeof(!!a));
+
+
+// console.log(Boolean(null));
+// console.log(Boolean(NaN));
+// console.log(Boolean(""));
+// console.log(Boolean(0));
+// console.log(Boolean(false));
